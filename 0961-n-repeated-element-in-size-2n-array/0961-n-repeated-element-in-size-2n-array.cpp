21f8d3f0e2822256a17a83569d7826d0d399544f
Anup Ghosh <anupbsmrstu@gmail.com>
2024-03-06 11:33:17 +0600

    Time: 19 ms (90.67%), Space: 27 MB (78.38%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -3,8 +3,8 @@ public:
     int repeatedNTimes(vector<int>& nums) {
         map<int,int>mp;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-            if( mp[nums[i]]>1)
+            // mp[nums[i]]++;
+            if( ++mp[nums[i]]>1)
                 return nums[i];
         }
         return 1;
