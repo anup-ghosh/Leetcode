bfe5140994a98d491f8e744f77cb949accb68fa8
Anup Ghosh <anupbsmrstu@gmail.com>
2024-04-24 07:56:00 +0600

    Time: 2 ms (50.95%), Space: 7.1 MB (81.41%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int tribonacci(int n) {
         int arr[38] = {0, 1, 1};
-        for(int i=3; i<sizeof(arr)/sizeof(int); i++) {
+        for(int i=3; i<=n; i++) {
             arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
         }
         return arr[n];
